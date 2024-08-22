95a117d6be6d58a068d97eab357e3c4ee771b155
Carlos Tirado <mariotiradotovar@outlook.com>
2024-08-21 22:12:59 -0500

    ..

--- schemas/usersSchema.js
+++ schemas/usersSchema.js
@@ -12,42 +12,42 @@ const validatorParamsUserId = [
 
 // Validación de cuerpo para crear un usuario
 const validatorBodyCreateUser = [
-    body("numero_identificacion")
+    body("identification_number")
         .exists()
         .withMessage("El número de identificación es obligatorio")
         .isNumeric()
         .withMessage("El número de identificación debe ser numérico"),
-    body("tipo_documento")
+    body("document_type")
         .exists()
         .withMessage("El tipo de documento es obligatorio")
         .isNumeric()
         .withMessage("El tipo de documento debe ser numérico"),
-    body("nombre")
+    body("name")
         .exists()
         .withMessage("El nombre es obligatorio")
         .isString()
         .withMessage("El nombre debe ser una cadena de texto"),
-    body("apellido")
+    body("last_name")
         .exists()
         .withMessage("El apellido es obligatorio")
         .isString()
         .withMessage("El apellido debe ser una cadena de texto"),
-    body("correo")
+    body("email")
         .exists()
         .withMessage("El correo es obligatorio")
         .isEmail()
         .withMessage("El correo debe ser un correo válido"),
-    body("telefono")
+    body("phone")
         .exists()
         .withMessage("El teléfono es obligatorio")
         .isNumeric()
         .withMessage("El teléfono debe ser numérico"),
-    body("nacionalidad")
+    body("origin")
         .exists()
         .withMessage("La nacionalidad es obligatoria")
         .isString()
         .withMessage("La nacionalidad debe ser una cadena de texto"),
-    body("direccion")
+    body("address")
         .exists()
         .withMessage("La dirección es obligatoria")
         .isString()
